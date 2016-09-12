51be5eb9ffffd685520d60ec0970f7f52a972c81
Juan Manuel Cordoba <juanmaco-97@hotmail.com>
2016-09-12 10:30:27 -0500

    modificado

--- src/Ejecutable.java
+++ src/Ejecutable.java
@@ -2,8 +2,13 @@
 public class Ejecutable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Primer commit");
+		
+	}
+	public void setup(){
+		
+	}
+	public void draw(){
+		
 	}
 
 }
